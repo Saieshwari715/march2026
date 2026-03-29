1b9d86e5ed5ed5084b8638940efbe0d3496eab06
Saieshwari715 <gajulasaieshwari0131@gmail.com>
2026-03-29 15:48:27 +0530

    Time: 8254 ms (5.05%), Space: 21.2 MB (24.85%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        seen=set()
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+               
+                if i!=j and abs(nums[i]-nums[j])==k:
+                    p=tuple(sorted([nums[i],nums[j]]))
+                    seen.add(p)
+
+        return len(seen)
+
+        
